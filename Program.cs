394c41a0aafcc1d9b747d1a56cff6fe6beeea14e
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-08 18:14:56 -0300

    feat: make TodoDbContext

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 
- // program.cs
+// program.cs
+using TodoAPI.AppDataContext;
+using TodoAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +11,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// User defined DI
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings")); // Add this line
+builder.Services.AddSingleton<TodoDbContext>(); // Add this line
+
+
 var app = builder.Build();
+
+{
+    using var scope = app.Services.CreateScope(); // Add this line
+    var context = scope.ServiceProvider; // Add this line
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
