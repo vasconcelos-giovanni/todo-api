b85a6a2a358562b8700afc903034a09513c90470
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-08 18:56:16 -0300

    feat: make ITodoServices

---
+++ Interfaces/ITodoServices.cs
@@ -0,0 +1,14 @@
+using TodoAPI.Contracts;
+using TodoAPI.Models;
+
+namespace TodoAPI.Interface
+{
+    public interface ITodoServices
+    {
+        Task<IEnumerable<Todo>> GetAllAsync();
+        Task<Todo> GetByIdAsync(Guid id);
+        Task CreateTodoAsync(CreateTodoRequest request);
+        Task UpdateTodoAsync(Guid id, UpdateTodoRequest request);
+        Task DeleteTodoAsync(Guid id);
+    }
+}
