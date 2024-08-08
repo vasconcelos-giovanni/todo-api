cc69684002f09b36c86e40e0a9e2edb577ad6d7d
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-08 18:20:52 -0300

    fix: add namespace to Todo DTOs

--- Contracts/UpdateTodoRequest.cs
+++ Contracts/UpdateTodoRequest.cs
@@ -1,22 +1,25 @@
 using System.ComponentModel.DataAnnotations;
 
-public class UpdateTodoRequest
+namespace TodoAPI.Contracts
 {
-    [StringLength(100)]
-    public string Title { get; set; }
-
-    [StringLength(500)]
-    public string Description { get; set; }
-
-    public bool? IsComplete { get; set; }
-
-    public DateTime? DueDate { get; set; }
-
-    [Range(1, 5)]
-    public int? Priority { get; set; }
-
-    public UpdateTodoRequest()
+    public class UpdateTodoRequest
     {
-        IsComplete = false;
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public bool? IsComplete { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        [Range(1, 5)]
+        public int? Priority { get; set; }
+
+        public UpdateTodoRequest()
+        {
+            IsComplete = false;
+        }
     }
 }
