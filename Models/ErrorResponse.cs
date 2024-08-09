3f78a869712855b6d7278ff5820ebe42c6d8f321
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-09 18:41:06 -0300

    fix: change ErrorResponse.Status code from string to int

--- Models/ErrorResponse.cs
+++ Models/ErrorResponse.cs
@@ -3,7 +3,7 @@ namespace TodoAPI.Models
     public class ErrorResponse
     {
         public string Title { get; set; }
-        public string StatusCode { get; set; }
+        public int StatusCode { get; set; }
         public string Message { get; set; }
     }
 }
