bcc4a6cd91e4eb52a9dd7d89a1051dd0f27a33c7
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-12 16:11:09 -0300

    feat: make index and create controller methods

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Contracts;
 using TodoAPI.Interface;
 
 namespace TodoAPI.Controllers
@@ -22,6 +23,48 @@ namespace TodoAPI.Controllers
         }
         /* -------------------------------------------------------------------------- */
 
-        
+        /* -------------------------------------------------------------------------- */
+        /*                                   Methods                                  */
+        /* -------------------------------------------------------------------------- */
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var todo = await _todoServices.GetAllAsync();
+                if (todo == null || !todo.Any())
+                {
+                    return Ok(new { message = "No Todo Items  found" });
+                }
+
+                string test = "TEST";
+
+                return Ok(new { message = "Successfully retrieved all todos.", data = todo });
+            }
+            catch (Exception ex)
+            {                
+                return StatusCode(500, new { message = "An error occurred while retrieving all todos.", error = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTodoAsync(CreateTodoRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _todoServices.CreateTodoAsync(request);
+                return Ok(new {message = "Todo created successfully"});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while creating the  crating Todo Item", error = ex.Message });
+            }
+        }
+        /* -------------------------------------------------------------------------- */
     }
 }
